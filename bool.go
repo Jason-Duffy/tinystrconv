07c59bfd89e95f7b457d2461e21c735cb6565568
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2024-07-07 15:54:18 +0100

    added banner comments

--- bool.go
+++ bool.go
@@ -1,8 +1,30 @@
+// =============================================================================
+// Project: tinystrconv
+// File: bool.go
+// Description: Conversion functions for boolean values.
+// Datasheet/Docs:
+//
+// Author: Jason Duffy
+// Created on: 06/07/2024
+//
+// Copyright: (C) 2024, Jason Duffy
+// License: See LICENSE file in the project root for full license information.
+// Disclaimer: See DISCLAIMER file in the project root for full disclaimer.
+// =============================================================================
+
+// -------------------------------------------------------------------------- //
+//                               Import Statement                             //
+// -------------------------------------------------------------------------- //
+
 package tinystrconv
 
 import (
 	"errors"
 )
+
+// -------------------------------------------------------------------------- //
+//                              Public Functions                              //
+// -------------------------------------------------------------------------- //
 
 // BoolToString converts a boolean to its string representation.
 func BoolToString(boolValue bool) string {
